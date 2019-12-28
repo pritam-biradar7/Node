3326469c0fb443d3d1eaf70172a55d6c75463e69
pritam-biradar7 <59283236+pritam-biradar7@users.noreply.github.com>
2019-12-28 19:21:41 +0530

    Add files via upload

---
+++ helloserver.js
@@ -0,0 +1,18 @@
+var http=require("http");
+serv=http.createServer(function(req,resp){
+resp.write("Hello World!!");
+console.log("request is received");
+resp.end("hi");
+});
+serv.listen(4000);
+console.log("server running at port 4000");
+
+/*var http=require('http');
+serv=http.createServer(function(req,resp){
+  resp.write("Hello World!");
+  console.log("request is received")
+  resp.end("Welcome to Node server!!!");
+
+});
+serv.listen(3000);
+console.log("server running at port 3000");*/
